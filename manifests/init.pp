a0bb096c64b28def7b9124140230a75baa85ee87
ROOK <donovan.rooks@swiss-as.com>
2025-03-24 16:26:02 +0100

    use Stdlib::Compat::Absolute_path instead of validate_absolute_path()
    wip

--- manifests/init.pp
+++ manifests/init.pp
@@ -524,7 +524,9 @@ class ssh (
   }
 
   if $sshd_config_xauth_location_real != undef {
-    validate_absolute_path($sshd_config_xauth_location_real)
+    if !($sshd_config_xauth_location_real =~ Stdlib::Compat::Absolute_path) {
+      fail("Expected an absolute path, got '${sshd_config_xauth_location_real}'")
+    }
   }
 
   if $ssh_package_source == 'USE_DEFAULTS' {
@@ -534,7 +536,9 @@ class ssh (
   }
 
   if $ssh_package_source_real != undef {
-    validate_absolute_path($ssh_package_source_real)
+    if !($ssh_package_source_real =~ Stdlib::Compat::Absolute_path) {
+      fail("Expected an absolute path, got '${ssh_package_source_real}'")
+    }
   }
 
   if $ssh_package_adminfile == 'USE_DEFAULTS' {
@@ -544,7 +548,9 @@ class ssh (
   }
 
   if $ssh_package_adminfile_real != undef {
-    validate_absolute_path($ssh_package_adminfile_real)
+    if !($ssh_package_adminfile_real =~ Stdlib::Compat::Absolute_path) {
+      fail("Expected an absolute path, got '${ssh_package_adminfile_real}'")
+    }
   }
 
   if $sshd_config_use_dns == 'USE_DEFAULTS' {
@@ -653,7 +659,9 @@ class ssh (
     $sshd_config_hostkey_real = $default_sshd_config_hostkey
   } else {
     validate_array($sshd_config_hostkey)
-    validate_absolute_path($sshd_config_hostkey)
+    if !($sshd_config_hostkey =~ Stdlib::Compat::Absolute_path) {
+      fail("Expected an absolute path, got '${sshd_config_hostkey}'")
+    }
     $sshd_config_hostkey_real = $sshd_config_hostkey
   }
 
@@ -814,7 +822,9 @@ class ssh (
   if is_integer($sshd_client_alive_count_max) == false { fail("ssh::sshd_client_alive_count_max must be an integer and is set to <${sshd_client_alive_count_max}>.") }
 
   if $sshd_config_banner != 'none' {
-    validate_absolute_path($sshd_config_banner)
+    if !($sshd_config_banner =~ Stdlib::Compat::Absolute_path) {
+      fail("Expected an absolute path, got '${sshd_config_banner}'")
+    }
   }
   if $sshd_banner_content != undef and $sshd_config_banner == 'none' {
     fail('ssh::sshd_config_banner must be set to be able to use sshd_banner_content.')
@@ -868,7 +878,9 @@ class ssh (
   }
 
   if $sshd_config_chrootdirectory != undef {
-    validate_absolute_path($sshd_config_chrootdirectory)
+    if !($sshd_config_chrootdirectory =~ Stdlib::Compat::Absolute_path) {
+      fail("Expected an absolute path, got '${sshd_config_chrootdirectory}'")
+    }
   }
 
   if $sshd_config_forcecommand != undef {
@@ -876,7 +888,9 @@ class ssh (
   }
 
   if $sshd_authorized_keys_command != undef {
-    validate_absolute_path($sshd_authorized_keys_command)
+    if !($sshd_authorized_keys_command =~ Stdlib::Compat::Absolute_path) {
+      fail("Expected an absolute path, got '${sshd_authorized_keys_command}'")
+    }
   }
 
   if $sshd_authorized_keys_command_user != undef {
@@ -973,12 +987,16 @@ class ssh (
     }
   }
 
-  validate_absolute_path($ssh_config_global_known_hosts_file)
+  if !($ssh_config_global_known_hosts_file =~ Stdlib::Compat::Absolute_path) {
+    fail("Expected an absolute path, got '${ssh_config_global_known_hosts_file}'")
+  }
   $ssh_config_global_known_hosts_file_real = any2array($ssh_config_global_known_hosts_file)
 
   if $ssh_config_global_known_hosts_list != undef {
     validate_array($ssh_config_global_known_hosts_list)
-    validate_absolute_path($ssh_config_global_known_hosts_list)
+    if !($ssh_config_global_known_hosts_list =~ Stdlib::Compat::Absolute_path) {
+      fail("Expected an absolute path, got '${ssh_config_global_known_hosts_list}'")
+    }
     $ssh_config_global_known_hosts_list_real = concat($ssh_config_global_known_hosts_file_real, $ssh_config_global_known_hosts_list)
   } else {
     $ssh_config_global_known_hosts_list_real = $ssh_config_global_known_hosts_file_real
@@ -1087,19 +1105,25 @@ class ssh (
     if is_array($sshd_config_hostcertificate_real) {
       validate_array($sshd_config_hostcertificate_real)
     }
-    validate_absolute_path($sshd_config_hostcertificate_real)
+    if !($sshd_config_hostcertificate_real =~ Stdlib::Compat::Absolute_path) {
+      fail("Expected an absolute path, got '${sshd_config_hostcertificate_real}'")
+    }
   }
 
   if $sshd_config_trustedusercakeys_real != undef {
     # TrustedUserCAKeys may be a path to the keys or 'none'
     if $sshd_config_trustedusercakeys_real != 'none' {
-      validate_absolute_path($sshd_config_trustedusercakeys_real)
+      if !($sshd_config_trustedusercakeys_real =~ Stdlib::Compat::Absolute_path) {
+        fail("Expected an absolute path, got '${sshd_config_trustedusercakeys_real}'")
+      }
     }
   }
   if $sshd_config_key_revocation_list_real != undef {
     # RevokedKeys may be a path to the key revocation list or 'none'
     if $sshd_config_key_revocation_list_real != 'none' {
-      validate_absolute_path($sshd_config_key_revocation_list)
+      if !($sshd_config_key_revocation_list =~ Stdlib::Compat::Absolute_path) {
+        fail("Expected an absolute path, got '${sshd_config_key_revocation_list}'")
+      }
     }
   }
 
